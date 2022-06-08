fa225a9ad131c7eb726e573c83bedbc3ba85955d
BAM-hub <bsharamin12@gmail.com>
2022-06-08 12:20:02 +0300

    first commit project setup backend

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const colors = require('colors');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+
+// connect to database
+connectDB();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === 'development'
+}));
+
+app.listen(port, console.log(`server running on port ${port}`))
