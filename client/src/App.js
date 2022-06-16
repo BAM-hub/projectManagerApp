9acc2594d78ab006470b380a38c5b9f0024857c0
BAM-hub <bsharamin12@gmail.com>
2022-06-16 03:42:57 +0300

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import Header from "./components/Header";
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-import Clients from "./components/Clients";
-import AddClientModal from "./components/AddClientModal";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -10,34 +12,38 @@ const cache = new InMemoryCache({
         clients: {
           merge(existing, incoming) {
             return incoming;
-          }
+          },
         },
         projects: {
           merge(existing, incoming) {
             return incoming;
-          }
-        }
-      }
-    }
-  }
+          },
+        },
+      },
+    },
+  },
 });
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: "http://localhost:5000/graphql",
   cache,
 });
-
 
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <Header />
-      <div className="container">
-        <AddClientModal />
-        <Clients />
-      </div>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/projects/:id' element={<Project />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </div>
+      </Router>
     </ApolloProvider>
   );
-}
+};
 
 export default App;
